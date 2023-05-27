caccaa4d732a1e9b535f4a73cf2cc60865cf2fe1
Manuel <mfernandezco.pex@sanitas.es>
2023-05-27 20:35:36 +0200

    first commit

--- src/main/java/com/example/akkodis/infraestructure/service/PriceServiceImpl.java
+++ src/main/java/com/example/akkodis/infraestructure/service/PriceServiceImpl.java
@@ -5,7 +5,9 @@ import com.example.akkodis.infraestructure.repository.IpricesRepository;
 import com.example.akkodis.application.utils.Tools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Comparator;
+
 
 @Service
 public class PriceServiceImpl implements PriceService {
@@ -14,14 +16,11 @@ public class PriceServiceImpl implements PriceService {
 
     @Override
     public PricesEntity getPriceByFilters(String date, int productId, int brandId) {
+        LocalDateTime targetDate = Tools.convertToDate(date);
 
-        List<PricesEntity> prices = repository.findByBrandIdAndProductId(productId, brandId).stream()
-            .filter(p -> Tools.convertToDate(p.getEnd_Date()).isAfter(Tools.convertToDate(date))
-                && Tools.convertToDate(p.getStart_Date()).isBefore(Tools.convertToDate(date)))
-            .sorted()
-            .toList();
-
-        return !prices.isEmpty() ? prices.get(0) : null;
+        return repository.findByBrandIdAndProductId(productId, brandId).stream()
+            .filter(p -> Tools.convertToDate(p.getEnd_Date()).isAfter(targetDate))
+            .max(Comparator.comparing(p -> Tools.convertToDate(p.getStart_Date())))
+            .orElse(null);
     }
-
 }
