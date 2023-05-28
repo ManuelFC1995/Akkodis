dfb0875899af5dcbe40889af459a5066da57c031
Manuel <mfernandezco.pex@sanitas.es>
2023-05-29 00:44:28 +0200

    first commit

--- src/test/groovy/AppControllerTest.groovy
+++ src/test/groovy/AppControllerTest.groovy
@@ -29,44 +29,45 @@ import javax.transaction.Transactional
 class AppControllerTest extends Specification {
 
     @Shared
-    ProductService productService = Mock()
+    ProductService productService
 
     @Subject
     AppController appController
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate = Mock()
 
     def setup() {
+        productService = Mock()
         appController = new AppController(productService)
-        // Insert data into the database before running any test
-        jdbcTemplate.execute("INSERT INTO prices (BRAND_ID, PRICE_LIST, PRODUCT_ID, PRICE, START_DATE, END_DATE, CURR, PRIORITY) VALUES (1, 1, 35455, 35.50, '2020-06-14 00:00:00', '2020-12-31 23:59:59', 'EUR', 0)")
-        jdbcTemplate.execute("INSERT INTO prices (BRAND_ID, PRICE_LIST, PRODUCT_ID, PRICE, START_DATE, END_DATE, CURR, PRIORITY) VALUES (1, 2, 35455, 25.45, '2020-06-14 15:00:00', '2020-06-14 18:30:00', 'EUR', 1)")
-        jdbcTemplate.execute("INSERT INTO prices (BRAND_ID, PRICE_LIST, PRODUCT_ID, PRICE, START_DATE, END_DATE, CURR, PRIORITY) VALUES (1, 3, 35455, 30.50, '2020-06-15 00:00:00', '2020-06-15 11:00:00', 'EUR', 1)")
-        jdbcTemplate.execute("INSERT INTO prices (BRAND_ID, PRICE_LIST, PRODUCT_ID, PRICE, START_DATE, END_DATE, CURR, PRIORITY) VALUES (1, 4, 35455, 38.95, '2020-06-15 16:00:00', '2020-12-31 23:59:59', 'EUR', 1)")
     }
+
 
     @Unroll("should return prices when valid request is made #index")
     def "should return prices for valid requests"() {
         given:
         def request = new ProductRequest(productId: 35455, brandId: 1, date: date)
         def expectedPrices = new Prices()
+        expectedPrices.setProductId(35455)
+        expectedPrices.setBrandId(1)
+        expectedPrices.setPrice(price)
         productService.getProduct(request.date, request.productId, request.brandId) >> expectedPrices
 
         when:
         ResponseEntity<Prices> response = appController.getPrice(request)
 
         then:
-        response.statusCode == HttpStatus.NO_CONTENT
+
+        response.statusCode == HttpStatus.OK
+        response.getBody().getPrice() == expectedPrices.getPrice()
+
 
         where:
-        index | date
-        1     | "2020-06-14 19:00:00"
-        2     | "2020-06-14 10:00:00"
-        3     | "2020-06-14 16:00:00"
-        4     | "2020-06-14 21:00:00"
-        5     | "2020-06-15 10:00:00"
-        6     | "2020-06-16 21:00:00"
+        index | date                  | price
+        1     | "2020-06-14 19:00:00" | 35.5
+        2     | "2020-06-14 10:00:00" | 35.5
+        3     | "2020-06-14 16:00:00" | 25.45
+        4     | "2020-06-14 21:00:00" | 35.5
+        5     | "2020-06-15 10:00:00" | 30.5
+        6     | "2020-06-16 21:00:00" | 38.95
     }
 
 
